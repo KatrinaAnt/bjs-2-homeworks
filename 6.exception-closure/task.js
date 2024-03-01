d754eb8af06542dbb95375e42ec43c445f80216f
Ekaterina Antonova <missis.katia-antonova@yandex.ru>
2024-03-01 04:23:38 +0300

    Домашнее задание реализовано

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(count) {
+	const resultParse = Number.parseFloat(count);
+	if (Number.isNaN(resultParse)) {
+		throw new Error("Невалидное значение");
+	};
+	return resultParse;
+}
+
+function validateCount(count) {
+	try {
+		return parseCount(count);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(first, second, third) {
+		if ((first + second) < third || (first + third) < second || (third + second) < first) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.firstSide = first;
+		this.secondSide = second;
+		this.thirdSide = third;
+	}
+
+	get perimeter() {
+		return (this.firstSide + this.secondSide + this.thirdSide);
+	}
+
+	get area() {
+		const halfmeter = this.perimeter / 2;
+		const areaValue = Math.sqrt(halfmeter * (halfmeter - this.firstSide) * (halfmeter - this.secondSide) * (halfmeter - this.thirdSide));
+		return Number(areaValue.toFixed(3));
+	}
+}
+
+function getTriangle(first, second, third) {
+	try {
+		return new Triangle(first, second, third);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return ("Ошибка! Треугольник не существует");
+			},
+			get area() {
+				return ("Ошибка! Треугольник не существует");
+			}
+		}
+	}
+}
